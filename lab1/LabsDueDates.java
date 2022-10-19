33f77cc657c3267167d257a3b1aefa5e148618f3
Siobhan Ebert <ssiobhan.ebert@icloud.com>
2022-10-18 21:35:50 -0400

    untrack .class files and rename java program

--- lab1/LabsDueDates.java
+++ lab1/LabsDueDates.java
@@ -3,7 +3,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.time.temporal.ChronoUnit;
 
-public class LabsDueDatesEdit {
+public class LabsDueDates {
 
 	public static void main(String[] args) {
 		try {
